7a990e3d3cd243180184ac6f0aa4f9635ea57630
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-05-04 23:01:57 +0200

    with graphql test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 const port = 3006;
@@ -7,4 +9,17 @@ app.get('/', (req, res) => {
 	res.send('<h3>Site works.</h3>');
 });
 
+const root = { hello: () => "This is hello." };
+
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	rootValue: root,
+	graphiql: true
+}));
+
 app.listen(port, () => console.log(`server is running at http://localhost:${port}`));
+
+// query in graphiql like this:
+// query {
+//  hello
+// }
